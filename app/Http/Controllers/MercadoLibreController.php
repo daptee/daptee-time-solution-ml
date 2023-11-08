f8f1ec515500358a1da029f231e733d458c07f2c
EnzoAmarilla <enzo100amarilla@gmail.com>
2023-11-08 12:25:59 -0300

    funcionalidades stock, estado y precio

--- app/Http/Controllers/MercadoLibreController.php
+++ app/Http/Controllers/MercadoLibreController.php
@@ -21,46 +21,63 @@ class MercadoLibreController extends Controller
             $user_id = $data["user_id"];
             $resource = $data["resource"];
             $order = Order::where("resource", $resource)->first();
+            $user = User::where("user_id", $user_id)->first();
 
             if(!$order){
 
-                $user = User::where("user_id", $user_id)->first();
                 if($user){
                     try {
                         $url = "https://api.mercadolibre.com" . $resource; // /orders/2000006733067046";
                         
-                        $response = Http::withHeaders([
-                            'Authorization' => 'Bearer ' . $user->access_token,
-                        ])->get($url);
+                        $response = Http::withHeaders([ 'Authorization' => 'Bearer ' . $user->access_token ])->get($url);
                 
                         $data = $response->json();
-                        Log::debug(["status"=>$response->status()]);
-                        if($response->status() == 401){
-                            if($data["message"] == "invalid_token"){
-                                $new_token = $this->refreshToken($user);
-                                $response = Http::withHeaders([
-                                    'Authorization' => 'Bearer ' . $new_token,
-                                ])->get($url);   
 
-                                $data = $response->json();
-                            }
+                        if($response->status() == 401){
+                            $new_token = $this->refreshToken($user);
+                            $response = Http::withHeaders(['Authorization' => 'Bearer ' . $new_token])->get($url);   
+                            $data = $response->json();
                         }
 
                         $new_order = $this->new_order($user_id, $resource, $data);
                     
                     } catch (\Exception $e) {
-                        Log::debug(["message" => "Error al registrar venta", "error" => $e->getMessage(), $e->getLine()]);
-                        return response()->json(["message" => "Error al registrar venta", "error" => $e->getMessage(), "line" => $e->getLine()], 500);
+                        Log::debug(["message" => "Error al registrar pedido", "error" => $e->getMessage(), $e->getLine()]);
+                        return response()->json(["message" => "Error al registrar pedido", "error" => $e->getMessage(), "line" => $e->getLine()], 500);
                     }
                 }
 
+                return response()->json(["message" => "Pedido guardado exitosamente.", "order" => $new_order]);
+
+            }else{
+
+                if($user){
+                    try {
+                        $url = "https://api.mercadolibre.com" . $resource; // /orders/2000006733067046";
+                        
+                        $response = Http::withHeaders([ 'Authorization' => 'Bearer ' . $user->access_token ])->get($url);
+                
+                        $data = $response->json();
+
+                        if($response->status() == 401){
+                            $new_token = $this->refreshToken($user);
+                            $response = Http::withHeaders(['Authorization' => 'Bearer ' . $new_token])->get($url);   
+                            $data = $response->json();
+                        }
+
+                        $order->data = $data;
+                        $order->save();
+                    
+                    } catch (\Exception $e) {
+                        Log::debug(["message" => "Error al actualizar pedido", "error" => $e->getMessage(), $e->getLine()]);
+                        return response()->json(["message" => "Error al actualizar pedido ", "error" => $e->getMessage(), "line" => $e->getLine()], 500);
+                    }
+                }
+
+                return response()->json(["message" => "Pedido actulizado exitosamente.", "order" => $order]);
             }
-            // else{
-                // return response()->json(["message" => "Orden ya procesada y guardada."], 500);
-            // }
         }
         
-        return response()->json(["message" => "Venta guardada exitosamente.", "order" => $new_order]);
     }
 
     public function refreshToken($user)
@@ -83,13 +100,13 @@ class MercadoLibreController extends Controller
 
             // Obtener la respuesta en formato JSON
             $data = $response->json();
-
             $user->access_token = $data["access_token"];
             $user->refresh_token = $data["refresh_token"];
             $user->save();
 
             return $data["access_token"];
         } catch (\Exception $e) {
+            Log::debug(["message" => "Error al actualizar token", "error" => $e->getMessage(), $e->getLine()]);
             return response()->json(["error" => $e->getMessage()], 500);
         }
     }
@@ -108,5 +125,137 @@ class MercadoLibreController extends Controller
             Log::debug(["message" => "Error al registrar venta (funcion principal)", "error" => $e->getMessage(), $e->getLine()]);
         }
         return $new_order; 
+    }
+
+    public function update_publication_price(Request $request)
+    {
+        $request->validate([
+            "item_id" => "required",
+            "user_id" => "required|numeric",
+            "price" => "required|numeric",
+        ]);
+
+        $user = User::where("user_id", $request->user_id)->first();
+        
+        if(!$user)
+            return response()->json(["message" => "Usuario no existente ID invalido"], 500);
+
+        try {
+            // Ejecutar endpoint para actualizar valor
+            $url = "https://api.mercadolibre.com/items/" . $request->item_id; // /orders/2000006733067046";
+            
+            $response = Http::withHeaders(['Authorization' => 'Bearer ' . $user->access_token])
+                        ->put($url, ['price' => $request->price]);
+    
+            $data = $response->json();
+
+            // return $response->status();
+            // Si devuelve 401 ejecutar refresh token en caso contrario seguir y hacer logica 
+            if($response->status() == 401){
+                $new_token = $this->refreshToken($user);
+                $response = Http::withHeaders(['Authorization' => 'Bearer ' . $new_token])
+                            ->put($url, ['price' => $request->price]); 
+
+                $data = $response->json();
+            }
+
+            // No guardamos en ningun lado ese cambio de precio o algo por el estilo? CHEQUEAR CON SEBA
+            // $new_order = $this->new_order($user_id, $resource, $data);
+        
+        } catch (\Exception $e) {
+            Log::debug(["message" => "Error al actualizar precio de publicación", "error" => $e->getMessage(), $e->getLine()]);
+            return response()->json(["message" => "Error al actualizar precio de publicación", "error" => $e->getMessage(), "line" => $e->getLine()], 500);
+        }
+
+        return response()->json(["message" => "Precio de publicación actualizado exitosamente.", "data" => $data]);
+    }
+
+    public function update_publication_status(Request $request)
+    {
+        $request->validate([
+            "item_id" => "required",
+            "user_id" => "required|numeric",
+            "status" => "required",
+        ]);
+
+        $user = User::where("user_id", $request->user_id)->first();
+        
+        if(!$user)
+            return response()->json(["message" => "Usuario no existente ID invalido"], 500);
+
+        try {
+            // Ejecutar endpoint para actualizar valor
+            $url = "https://api.mercadolibre.com/items/" . $request->item_id; // /orders/2000006733067046";
+            
+            $response = Http::withHeaders(['Authorization' => 'Bearer ' . $user->access_token])
+                        ->put($url, ['status' => $request->status]);
+    
+            $data = $response->json();
+
+            // Si devuelve 401 ejecutar refresh token en caso contrario seguir y hacer logica 
+            if($response->status() == 401){
+                $new_token = $this->refreshToken($user);
+                $response = Http::withHeaders(['Authorization' => 'Bearer ' . $new_token])
+                            ->put($url, ['status' => $request->status]); 
+
+                $data = $response->json();
+            }
+
+            // No guardamos en ningun lado ese cambio de estado o algo por el estilo? CHEQUEAR CON SEBA
+            // $new_order = $this->new_order($user_id, $resource, $data);
+        
+        } catch (\Exception $e) {
+            Log::debug(["message" => "Error al actualizar estado de publicación", "error" => $e->getMessage(), $e->getLine()]);
+            return response()->json(["message" => "Error al actualizar estado de publicación", "error" => $e->getMessage(), "line" => $e->getLine()], 500);
+        }
+
+        return response()->json(["message" => "Estado de publicación actualizado exitosamente.", "data" => $data]);
+    }
+
+    public function update_publication_stock(Request $request)
+    {
+        $request->validate([
+            "item_id" => "required",
+            "user_id" => "required|numeric",
+            "stock" => "required|numeric",
+        ]);
+
+        $user = User::where("user_id", $request->user_id)->first();
+        
+        if(!$user)
+            return response()->json(["message" => "Usuario no existente ID invalido"], 500);
+
+        try {
+            // Ejecutar endpoint para actualizar valor
+            $url = "https://api.mercadolibre.com/items/" . $request->item_id; // /orders/2000006733067046";
+            
+            $response = Http::withHeaders(['Authorization' => 'Bearer ' . $user->access_token])
+                        ->put($url, ['available_quantity' => $request->stock]);
+    
+            $data = $response->json();
+
+            // Si devuelve 401 ejecutar refresh token en caso contrario seguir y hacer logica 
+            if($response->status() == 401){
+                $new_token = $this->refreshToken($user);
+                $response = Http::withHeaders(['Authorization' => 'Bearer ' . $new_token])
+                            ->put($url, ['available_quantity' => $request->stock]); 
+
+                $data = $response->json();
+            }
+
+            // No guardamos en ningun lado este cambio de stock o algo por el estilo? CHEQUEAR CON SEBA
+            // $new_order = $this->new_order($user_id, $resource, $data);
+        
+        } catch (\Exception $e) {
+            Log::debug(["message" => "Error al actualizar stock de publicación", "error" => $e->getMessage(), $e->getLine()]);
+            return response()->json(["message" => "Error al actualizar stock de publicación", "error" => $e->getMessage(), "line" => $e->getLine()], 500);
+        }
+
+        return response()->json(["message" => "Stock de publicación actualizado exitosamente.", "data" => $data]);
+    }
+
+    public function test_api()
+    {
+        dd("Test api correcto");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MercadoLibreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/notificacion', [MercadoLibreController::class, 'handleWebhook']);
+Route::post('/publication/update/price', [MercadoLibreController::class, 'update_publication_price']);
+Route::post('/publication/update/status', [MercadoLibreController::class, 'update_publication_status']);
+Route::post('/publication/update/stock', [MercadoLibreController::class, 'update_publication_stock']);
+Route::get('/test_api', [MercadoLibreController::class, 'test_api']);
